9814052825997314539c2236609114ba6fba3ac1
Dámsa Levente <damsalevente@gmail.com>
2018-03-09 09:45:51 +0100

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+//Életkor változáskor értesítést küld --> delegate
+
+namespace beagyki2
+{
+    // 1
+    public delegate void AgeChangingDelegate(int oldAge,
+                                            int newAge
+                                            );
+    [XmlRoot("Ember")]
+    public class Person
+    {
+
+        // 2
+        public event AgeChangingDelegate AgeChanging;
+
+        private int age;
+        [XmlAttribute("Eletkor")]
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException();
+                }
+                // 3 változás értesítés előtt
+                AgeChanging?.Invoke(age, value);    //Changed, if we do after age=value
+                age = value;
+            }
+        }
+        [XmlIgnore]
+        public string Name { get; set; }    // name+get,set in one line; private set to only read outside
+        public int SzuletesiEv              //read only property
+        {
+            get
+            {
+                return DateTime.Today.Year - age;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace beagyki2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Person p = new Person
+            {
+                Age = 13,
+                Name = "Hello"
+            };
+            /*
+            // p.AgeChanging = new AgeChangingDelegate(personAgeChanging);
+            // 5
+            p.AgeChanging += personAgeChanging;
+            //p.AgeChanging += personAgeChanging;
+            //p.AgeChanging += personAgeChanging;
+            //p.AgeChanging -= personAgeChanging;
+            ++p.Age;
+            Console.WriteLine(p.Name);
+            Console.WriteLine(p.Age);
+            Console.WriteLine(p.SzuletesiEv);
+            XmlSerializer ser = new XmlSerializer(typeof(Person));
+            var file = new FileStream("person.txt", FileMode.Create);
+            ser.Serialize(file, p);
+            file.Close();
+            System.Diagnostics.Process.Start("person.txt");
+            */
+            List<Person> people = new List<Person>();
+            people.Add(p);
+            people.Add(new Person() { Name = "AAA", Age = 43 });
+            foreach(Person e in people.FindAll(personFilter => personFilter.Age%2!=0))
+            {
+                Console.WriteLine(e.Name);
+            }
+            Console.ReadKey();
+        }
+        // 4 
+        static void personAgeChanging(int oldAge, int newAge)
+        {
+            Console.WriteLine($"Person age {oldAge} -> {newAge} ");
+        }
+        delegate bool personFilter(Person person);
+    }
+
+}
